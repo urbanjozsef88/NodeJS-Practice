961f3933301619b6e498868d499e55fb46e89622
unknown <jozsef.urban.88@gmail.com>
2020-06-05 18:56:31 +0200

    Skeleton

---
+++ ServerPrectice.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const userRouter = require('./routes/shop');
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(userRouter);
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>')
+})
+
+  
+app.listen(3000);
