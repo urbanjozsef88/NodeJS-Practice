811ab9c7dff1da04ad83ffad3116497a7b9afd87
unknown <jozsef.urban.88@gmail.com>
2020-06-05 22:31:09 +0200

    Basic css added

--- ServerPractice.js
+++ ServerPractice.js
@@ -3,16 +3,17 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const userRouter = require('./routes/shop');
 const path = require('path');
+const rootDir = require('./util/path');
 
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(userRouter);
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname,'views', 'page-not-found.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', 'page-not-found.html'));
 })
 
   
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const rootDir = require('../util/path');
 
 
 router.get('/shop',(req, res, next) => {
-res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
+res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 });
 
 
