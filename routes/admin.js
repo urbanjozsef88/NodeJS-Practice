2ad5ab13edb9e61a5b856d8ef634b2c2fab8dd56
unknown <jozsef.urban.88@gmail.com>
2020-06-06 21:28:22 +0200

    product controller basic settings done

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,9 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products')
+const productsController = require('../controllers/products');
 
 const router = express.Router();
-
-const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', productsController.getAddProduct);
@@ -14,5 +12,4 @@ router.get('/add-product', productsController.getAddProduct);
 // /admin/add-product => POST
 router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
