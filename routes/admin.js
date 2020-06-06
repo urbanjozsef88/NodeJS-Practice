9a13b346ec78c4d4731c726a5ed061e19b051cf2
unknown <jozsef.urban.88@gmail.com>
2020-06-06 21:20:50 +0200

    controllers added

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,18 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/products')
+
 const router = express.Router();
-const rootDir = require('../util/path');
 
-    router.get('/add-product', (req, res, next) => {
-        res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    })
-    
-    router.post('/add-product',(req, res, next) => {
-        console.log(req.body);
-        res.redirect('/');
-    })
+const products = [];
 
-module.exports = router;
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
+
+exports.routes = router;
+exports.products = products;
